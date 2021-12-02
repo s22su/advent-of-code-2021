7ee1d8662404ab83f1648b495e41bdcbb4b97654
Sergei Beregov <sergei.beregov@gmail.com>
2021-12-02 23:06:46 +0200

    Day 2: use tuple instead of map

--- lib/day02.ex
+++ lib/day02.ex
@@ -6,25 +6,23 @@ defmodule AdventOfCode.Day02 do
 
   def part1(input) do
     preprocess_input(input)
-    |> Enum.reduce(%{x: 0, y: 0}, fn instruction, acc -> move1(instruction, acc) end)
-    |> (fn %{x: x, y: y} -> x * y end).()
+    |> Enum.reduce({0, 0}, fn instruction, acc -> move1(instruction, acc) end)
+    |> (fn {x, y} -> x * y end).()
   end
 
   def part2(input) do
     preprocess_input(input)
-    |> Enum.reduce(%{x: 0, y: 0, aim: 0}, fn instruction, acc -> move2(instruction, acc) end)
-    |> (fn %{x: x, y: y} -> x * y end).()
+    |> Enum.reduce({0, 0, 0}, fn instruction, acc -> move2(instruction, acc) end)
+    |> (fn {x, y, _} -> x * y end).()
   end
 
-  defp move1(["up", delta], %{y: y} = curr), do: Map.put(curr, :y, y - delta)
-  defp move1(["down", delta], %{y: y} = curr), do: Map.put(curr, :y, y + delta)
-  defp move1(["forward", delta], %{x: x} = curr), do: Map.put(curr, :x, x + delta)
+  defp move1(["up", delta], {x, y}), do: {x, y - delta}
+  defp move1(["down", delta], {x, y}), do: {x, y + delta}
+  defp move1(["forward", delta], {x, y}), do: {x + delta, y}
 
-  defp move2(["up", delta], %{aim: aim} = curr), do: Map.put(curr, :aim, aim - delta)
-  defp move2(["down", delta], %{aim: aim} = curr), do: Map.put(curr, :aim, aim + delta)
-
-  defp move2(["forward", delta], %{x: x, y: y, aim: aim} = curr),
-    do: Map.merge(curr, %{x: x + delta, y: y + aim * delta})
+  defp move2(["up", delta], {x, y, aim}), do: {x, y, aim - delta}
+  defp move2(["down", delta], {x, y, aim}), do: {x, y, aim + delta}
+  defp move2(["forward", delta], {x, y, aim}), do: {x + delta, y + aim * delta, aim}
 
   direction = choice([string("up"), string("down"), string("forward")])
   delta = ascii_string([?0..?9], min: 1) |> map({String, :to_integer, []})
