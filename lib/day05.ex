8776e3d1c3d5d5cc3b6e0f9442f66ce6af0d2da4
Sergei Beregov <sergei.beregov@gmail.com>
2021-12-05 10:13:46 +0200

    Speed up day 5
    Realized that I can just count frequencies and skip "plotting" the lines
    completely.

--- lib/day05.ex
+++ lib/day05.ex
@@ -5,73 +5,44 @@ defmodule AdventOfCode.Day05 do
   import NimbleParsec
 
   def part1(input) do
-    lines =
-      preprocess_input(input)
-      |> Enum.map(fn [[x1, y1], [x2, y2]] ->
-        cond do
-          x1 == x2 ->
-            for y <- y1..y2, do: {x1, y}
+    preprocess_input(input)
+    |> Enum.map(fn [[x1, y1], [x2, y2]] ->
+      cond do
+        x1 == x2 ->
+          for y <- y1..y2, do: {x1, y}
 
-          y1 == y2 ->
-            for x <- x1..x2, do: {x, y1}
+        y1 == y2 ->
+          for x <- x1..x2, do: {x, y1}
 
-          true ->
-            nil
-        end
-      end)
-      |> Enum.reject(&is_nil/1)
-
-    lines
-    |> init_empty_board()
-    |> put_lines(lines)
+        true ->
+          nil
+      end
+    end)
+    |> Enum.reject(&is_nil/1)
+    |> List.flatten()
+    |> Enum.frequencies()
     |> Enum.filter(fn {_k, v} -> v > 1 end)
     |> Enum.count()
   end
 
   def part2(input) do
-    lines =
-      preprocess_input(input)
-      |> Enum.map(fn [[x1, y1], [x2, y2]] ->
-        cond do
-          x1 == x2 ->
-            for y <- y1..y2, do: {x1, y}
+    preprocess_input(input)
+    |> Enum.map(fn [[x1, y1], [x2, y2]] ->
+      cond do
+        x1 == x2 ->
+          for y <- y1..y2, do: {x1, y}
 
-          y1 == y2 ->
-            for x <- x1..x2, do: {x, y1}
+        y1 == y2 ->
+          for x <- x1..x2, do: {x, y1}
 
-          true ->
-            Enum.zip(x1..x2, y1..y2)
-        end
-      end)
-
-    lines
-    |> init_empty_board()
-    |> put_lines(lines)
+        true ->
+          Enum.zip(x1..x2, y1..y2)
+      end
+    end)
+    |> List.flatten()
+    |> Enum.frequencies()
     |> Enum.filter(fn {_k, v} -> v > 1 end)
     |> Enum.count()
-  end
-
-  defp put_lines(board, lines),
-    do: Enum.reduce(lines, board, fn line_coords, acc -> put_line(acc, line_coords) end)
-
-  defp put_line(board, coords),
-    do:
-      Enum.reduce(coords, board, fn {x, y}, acc -> Map.update!(acc, {x, y}, fn v -> v + 1 end) end)
-
-  defp init_empty_board(lines) do
-    {max_x, max_y} = find_max_xy(lines)
-
-    for(x <- 0..max_x, y <- 0..max_y, do: {x, y})
-    |> Enum.reduce(%{}, fn k, acc -> Map.put(acc, k, 0) end)
-  end
-
-  defp find_max_xy(coords) do
-    Enum.reduce(coords, {0, 0}, fn line, {max_x, max_y} ->
-      c_max_x = Enum.max_by(line, fn {x, _y} -> x end) |> elem(0)
-      c_max_y = Enum.max_by(line, fn {_x, y} -> y end) |> elem(1)
-
-      {Enum.max([max_x, c_max_x]), Enum.max([max_y, c_max_y])}
-    end)
   end
 
   number = ascii_string([?0..?9], min: 1) |> map({String, :to_integer, []})
